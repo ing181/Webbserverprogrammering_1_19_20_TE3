966921daee1e874fcdfcba49d123ad00536e5a81
ing181 <ingemar.ceicer@mark.se>
2019-09-12 09:26:35 +0200

    Lektion

---
+++ 2019-09-05/fartocel.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="sv">
+   <head>
+      <meta charset="utf-8" />
+      <title>PHP</title>
+   </head>
+   <body>
+	   <form action="fartocel.php" method="get">
+		   <input type="text" name="invarde">
+
+       <select name="val">
+            <option value="tocelsius">To Celsius</option>
+            <option value="tofahrenheit">To Fahrenheit</option>
+       </select>
+
+		   <input type="submit">
+
+	   </form>
+       <?php
+
+         if($_GET['val']=="tofahrenheit")
+         {
+           $invarde=$_GET['invarde']; // Tomt innan formuläret skickats
+           $fahrenheit = $invarde*9/5+32;
+           $fahrenheit = number_format($fahrenheit, 2,",","");
+           echo $invarde." Celsius = ".$fahrenheit." Fahrenheit";
+
+
+         }
+         else{
+           $invarde=$_GET['invarde']; // Tomt innan formuläret skickats
+           $celsius = ($invarde-32)*5/9;
+           $celsius = number_format($celsius, 2,",","");
+           echo $invarde." Fahrenheit = ".$celsius." Celsius";
+         }
+
+
+
+       ?>
+
+
+   </body>
+</html>
